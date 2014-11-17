4dc6363d1b9cae2bf972d8e6cf921968a4a6a083
Michael Kuehn <mkuehn10@gmail.com>
2014-11-17 07:17:24 -0500

    Update 01

--- 01_miles_to_feet.py
+++ 01_miles_to_feet.py
@@ -7,7 +7,6 @@
 ###################################################
 # Miles to feet conversion formula # Student should enter statement on the next line.
 
-print 5280 * 13
 
 ###################################################
 # Expected output # Student should look at the following comments and compare to printed output.
